64928059f60c7d64ec630a16b42d4a9140dc8fb6
danilBren <danilbren1@gmail.com>
2026-03-24 23:55:51 +0100

    change sbatch settings

--- experiments/dream_coder/job/pixels_true.sh
+++ experiments/dream_coder/job/pixels_true.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --account=research-eemcs-st
-#SBATCH --partition=memory
 #SBATCH --job-name=pixels-compression=true
+#SBATCH --partition=compute
+#SBATCH --time=01:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=32000MB
-#SBATCH --time=1-00:00:00
 #SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=3968MB
 #SBATCH --output=job_out/job_%j.out
 #SBATCH --error=job_out/job_%j.err
 
